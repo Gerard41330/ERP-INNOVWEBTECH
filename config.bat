8b1ad91a89486079da8d2fb846ff3dffb49235bf
Gerard41330 <simier.gerard@orange.fr>
2025-12-21 21:28:46 +0100

    chore: initial commit - architecture ERP InnovWebTech

---
+++ config.bat
@@ -0,0 +1,16 @@
+REM ========================================
+REM CONFIGURATION DE L'ENVIRONNEMENT
+REM ========================================
+
+REM Chemin vers WAMP (modifiez selon votre installation)
+REM Exemples courants:
+REM   C:\wamp64\wampmanager.exe
+REM   C:\wamp\wampmanager.exe
+REM   D:\wamp64\wampmanager.exe
+set WAMP_PATH=C:\wamp64\wampmanager.exe
+
+REM Temps d'attente pour le demarrage de WAMP (en secondes)
+set WAMP_WAIT_TIME=15
+
+REM Activer/desactiver le demarrage automatique de WAMP (1=oui, 0=non)
+set AUTO_START_WAMP=1
